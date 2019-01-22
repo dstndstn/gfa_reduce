e885c7b43efff0450e446fb8d1fa9bd28623f603
Aaron Meisner <ameisner@fas.harvard.edu>
2019-01-22 09:25:46 -0800

    starting to fill in common.py

--- py/common.py
+++ py/common.py
@@ -1,12 +1,46 @@
 import numpy as np
 
-def valid_image_extnum_list():
+def valid_image_extnum_list(fz=True):
     """ 
-    valid image extension numbers are 1-5 inclusive
-    I guess there is also a dummy zeroth extension...
+    When fzipped, valid image extension numbers are 1-5 inclusive.
+    When fzipped, there is also a dummy zeroth extension.
+    When not fzipped, the valid image extensions are 0-4 inclusive.
+    When not fzipped, there is no dummy zeroth extension.
+
+    astropy.io.fits has weird (buggy?) behavior for fzipped files:
+        - fits.getheader with ext=0 returns the dummy ext=0 header.
+        - fits.getdata with ext=0 and header=True returns the ext=1 header and
+          ext=1 image data.
+
+    I want image extension numbers that are valid for both getheader and
+    getdata, so for the fzipped case, I will assert that the valid image
+    extension numbers are 1-5 inclusive and that ext=0 is invalid.
+
+    hdul.info()
+    Filename: FORDK_NOBIN-00000001.fits.fz
+    No.    Name      Ver    Type      Cards   Dimensions   Format
+      0  CI            1 PrimaryHDU       15   ()
+      1  CIN           1 CompImageHDU     43   (3072, 2048)   int16
+      2  CIW           1 CompImageHDU     43   (3072, 2048)   int16
+      3  CIC           1 CompImageHDU     43   (2048, 3072)   int16
+      4  CIE           1 CompImageHDU     43   (3072, 2048)   int16
+      5  CIS           1 CompImageHDU     43   (3072, 2048)   int16
+
+    hdul.info()
+    Filename: FORDK_NOBIN-00000001.fits
+    No.    Name      Ver    Type      Cards   Dimensions   Format
+      0 CIN            1 PrimaryHDU      47   (3072, 2048)    int16
+      1 CIW            1 ImageHDU        42   (3072, 2048)    int16
+      2 CIC            1 ImageHDU        42   (2048, 3072)    int16
+      3 CIE            1 ImageHDU        42   (3072, 2048)    int16
+      4 CIS            1 ImageHDU        42   (3072, 2048)    int16
+
+    Note that the swapped dimensions of CIC above are a bug in the sample
+    data, and that the number of header cards will presumably be changing
+    relative to the preliminary sample data.
     """
 
-    return np.arange(1, 6)
+    return (np.arange(1, 6) if fz else np.arange(0, 5))
 
 def valid_image_extname_list():
     """ any reason to set order a specific way here? """
@@ -15,6 +49,8 @@ def valid_image_extname_list():
     return ['CIE', 'CIN', 'CIC', 'CIS', 'CIE']
 
 def valid_ci_number_list():
+    """ from DESI-3347 page 2 CI# labels """
+
     return np.arange(1, 6)
 
 def ci_extname_to_extnum(extname):
@@ -35,10 +71,10 @@ def ci_number_to_extnum():
 def ci_number_to_extname():
 
 
-def ci_boundary_coords():
+def ci_boundary_coords(pix_center=False):
 
 
-def ci_corner_coords():
+def ci_corner_coords(pix_center=False):
 
 
 def check_valid_extnum():
