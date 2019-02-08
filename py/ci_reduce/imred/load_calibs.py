4fd174f294d5ef5f89451bc58ea5bacad54e18af
Aaron Meisner <ameisner@fas.harvard.edu>
2019-02-08 00:20:15 -0800

    starting to implement imred sub-module capabilities

---
+++ py/ci_reduce/imred/load_calibs.py
@@ -0,0 +1,16 @@
+import ci_reduce.common as common
+import astropy.io.fits as fits
+import os
+
+def read_bias_image(ci_extname):
+    assert(common.is_valid_extname(ci_extname))
+
+    par = common.ci_misc_params()
+    bias_fname = os.path.join(os.environ['CI_REDUCE_ETC'], \
+                              par['master_bias_filename'])
+
+    assert(os.path.exists(bias_fname))
+
+    bias = fits.getdata(bias_fname, extname=ci_extname)
+
+    return bias
