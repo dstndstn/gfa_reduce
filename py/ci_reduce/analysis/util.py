1e8c8d0b3694ff8218a5b2665bafdd0d05f327eb
Aaron Meisner <ameisner@fas.harvard.edu>
2019-02-09 13:40:25 -0800

    initial

---
+++ py/ci_reduce/analysis/util.py
@@ -0,0 +1,28 @@
+import numpy as np
+import ci_reduce.common as common
+
+# should probably also have something available for the case of
+# upbinning, although not clear when I might actually ever use that
+def ci_downbinned_shape(binfac):
+    # assume integer rebinning until I come across a case where
+    # arbitrary rebinning would be valuable
+    # the native CI image dimensions are very favorable for integer rebinning
+
+    # assume same rebinning factor in both dimensions for now, until
+    # I come across a case where I would want otherwise
+
+    assert((type(binfac).__name__ == 'int') or binfac.is_integer())
+
+    par = common.ci_misc_params()
+
+    width_native = par['width_pix']
+    height_native = par['height_pix']
+
+    width_downbinned = float(width_native)/float(binfac)
+    height_downbinned = float(height_native)/float(binfac)
+
+    assert(width_downbinned.is_integer())
+    assert(height_downbinned.is_integer())
+
+    # note Python convention for (height, width)
+    return int(height_downbinned), int(width_downbinned)
