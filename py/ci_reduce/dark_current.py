c5a4080c46aeeaf9d83636315d98ad229d19a38b
Aaron Meisner <ameisner@fas.harvard.edu>
2019-02-09 06:32:59 -0800

    stub for file that will contain function returning dark current information

---
+++ py/ci_reduce/dark_current.py
@@ -0,0 +1,6 @@
+# return dark current rate in e-/pix/sec as a function of temperature
+# based on DESI-3358 slide 9, or else my own dark current measurements once 
+# I get access to the relevant engineering data 
+
+# once I get access to the engineering dark frames, would also be good
+# to construct *images* of the dark current rate
