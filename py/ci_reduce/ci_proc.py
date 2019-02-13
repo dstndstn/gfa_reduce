97015c335c5b47c9665926c351bc763984ea2c53
Aaron Meisner <ameisner@fas.harvard.edu>
2019-02-12 22:11:08 -0800

    initial

---
+++ py/ci_reduce/ci_proc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import ci_reduce.io as io
+
+if __name__ == "__main__":
+    descr = 'run full ci_reduce pipeline on a CI exposure'
+    parser = argparse.ArgumentParser(description=descr)
+    parser.add_argument('fname_in', type=str, nargs=1)
+
+    args = parser.parse_args()
+
+    fname_in = args.fname_in[0]
+
+    assert(os.path.exists(fname_in))
+
+    exp = io.load_exposure(fname_in)
