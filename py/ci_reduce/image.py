b64d77a3daf1a884a346655007b4a740cbea92a2
Aaron Meisner <ameisner@fas.harvard.edu>
2019-02-16 20:28:12 -0800

    add bitmask attribute and associated create_dq_mask() method

--- py/ci_reduce/image.py
+++ py/ci_reduce/image.py
@@ -1,5 +1,5 @@
-import astropy.io.fits as fits
 import ci_reduce.common as common
+import ci_reduce.imred.dq_mask as dq_mask
 from astropy import wcs
 
 class CI_image:
@@ -9,3 +9,11 @@ class CI_image:
         self.image = image
         self.header = header
         self.wcs = wcs.WCS(header)
+        # lazily compute bitmask image only as requested
+        self.bitmask = None
+
+    def create_dq_mask(self):
+        if self.bitmask is not None:
+            return
+
+        self.bitmask = dq_mask.dq_bitmask(self.image, self.header['EXTNAME'])
