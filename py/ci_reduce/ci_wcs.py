4f4c36dc7382cc5923943c5baa0108f96bb4877c
Aaron Meisner <ameisner@fas.harvard.edu>
2019-03-14 01:05:20 -0700

    use common.py to reduce hardcoding

--- py/ci_reduce/ci_wcs.py
+++ py/ci_reduce/ci_wcs.py
@@ -2,15 +2,16 @@ import astropy.io.fits as fits
 import numpy as np
 from astropy import wcs
 import os
+import ci_reduce.common as common
 
 def nominal_tan_wcs(telra, teldec, extname):
     # Create a new WCS object.  The number of axes must be set
     # from the start
 
-    # eventually avoid this hardcoding by appropriately
-    # updating/using common.py
-    fname = os.path.join(os.environ['CI_REDUCE_ETC'],
-                         'dummy_with_headers.bigtan.fits.gz')
+    par = common.ci_misc_params()
+
+    fname = os.path.join(os.environ[par['etc_env_var']],
+                         par['headers_dummy_filename'])
 
     h = fits.getheader(fname, extname=extname)
 
