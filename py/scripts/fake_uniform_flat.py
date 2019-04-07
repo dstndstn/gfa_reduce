8a3810881c528ac7d275182bafbf798965ba699b
Aaron Meisner <ameisner@fas.harvard.edu>
2019-04-07 07:16:42 -0700

    use a fake uniform flat until post-installation data needed to build actual flats has been acquired

---
+++ py/scripts/fake_uniform_flat.py
@@ -0,0 +1,19 @@
+import astropy.io.fits as fits
+import numpy as np
+import os
+
+fname_in = '/project/projectdirs/desi/users/ameisner/CI/deprecated_fordk_calibs/CI_master_flat.fits.gz'
+
+hdul = fits.open(fname_in)
+
+for hdu in hdul:
+    image = hdu.data
+    image.fill(1.0)
+
+    hdu.data = image
+
+outname = '/project/projectdirs/desi/users/ameisner/CI/ci_reduce_etc/fake_unfiorm_flat.fits'
+
+assert(not os.path.exists(outname))
+
+hdul.writeto(outname)
