7ff009f3cd30d1d60cff46101128a387d55f25a3
Aaron Meisner <ameisner@fas.harvard.edu>
2019-11-22 21:18:03 -0800

    refactor

--- py/ci_reduce/gfa_red.py
+++ py/ci_reduce/gfa_red.py
@@ -8,6 +8,85 @@ import ci_reduce.analysis.util as util
 import ci_reduce.common as common
 import ci_reduce.analysis.recalib_astrom as wcs
 
+def _proc(fname_in, outdir=None, careful_sky=False, no_cataloging=False,
+          no_gaia_xmatch=False, cube_index=None, skip_image_outputs=False):
+
+    print('Starting GFA reduction pipeline at: ' + str(datetime.utcnow()) + 
+          ' UTC')
+    
+    write_outputs = (outdir is not None)
+
+    assert(os.path.exists(fname_in))
+
+    gitrev = io.retrieve_git_rev()
+
+    if write_outputs:
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        # fail if ANY of expected outputs already exist
+        io.check_image_level_outputs_exist(outdir, fname_in, gzip=True,
+                                           cube_index=cube_index)
+
+    exp = io.load_exposure(fname_in, cube_index=cube_index)
+
+    print('Attempting to compute basic statistics of raw pixel data')
+    imstats = io.gather_pixel_stats(exp)
+
+    # create data quality bitmasks
+    exp.create_all_bitmasks()
+
+    # go from "raw" images to "reduced" images
+    exp.calibrate_pixels()
+
+    # calculate sky brightness in mag per sq asec
+    exp.estimate_all_sky_mags(careful_sky=careful_sky)
+    exp.estimate_all_sky_sigmas(careful_sky=careful_sky)
+
+    par = common.ci_misc_params()
+
+    if not no_cataloging:
+        catalogs = exp.all_source_catalogs()
+
+        for extname, cat in catalogs.items():
+            if cat is not None:
+                util.create_det_ids(cat, extname, fname_in,
+                                    cube_index=cube_index)
+
+        # reformat the output catalogs into a single merged astropy Table
+        catalog = io.combine_per_camera_catalogs(catalogs)
+
+        # run astrometric recalibration
+        print('Attempting astrometric recalibration relative to Gaia DR2')
+        astr = wcs.recalib_astrom(catalog, fname_in)
+        exp.update_wcs(astr)
+        exp.recompute_catalog_radec(catalog)
+        
+        if (not no_gaia_xmatch) and (par['gaia_env_var'] in os.environ):
+            print('Attempting to identify Gaia cross-matches')
+            catalog = io.append_gaia_crossmatches(catalog)
+        
+    # try to write image-level outputs if outdir is specified
+
+    if write_outputs:
+        if not skip_image_outputs:
+            print('Attempting to write image-level outputs to directory : ' + 
+                  outdir)
+            # could add command line arg for turning off gzip compression
+            io.write_image_level_outputs(exp, outdir, fname_in, gzip=True,
+                                         cube_index=cube_index)
+
+        if not no_cataloging:
+            io.write_exposure_source_catalog(catalog, outdir, fname_in,
+                                             cube_index=cube_index)
+        # make this work correctly in the case that --no_cataloging is set
+        io.write_ccds_table(imstats, catalog, exp, outdir, fname_in,
+                            cube_index=cube_index)
+
+    print('Successfully finished reducing ' + fname_in)
+
+    print('GFA reduction pipeline completed at: ' + str(datetime.utcnow()) + 
+          ' UTC')
+
 if __name__ == "__main__":
     descr = 'run full gfa_reduce pipeline on a GFA exposure'
     parser = argparse.ArgumentParser(description=descr)
@@ -34,82 +113,9 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
 
-    print('Starting GFA reduction pipeline at: ' + str(datetime.utcnow()) + 
-          ' UTC')
-
     fname_in = args.fname_in[0]
-    cube_index = args.cube_index
-    
-    write_outputs = (args.outdir is not None)
 
-    assert(os.path.exists(fname_in))
-
-    gitrev = io.retrieve_git_rev()
-
-    if write_outputs:
-        outdir = args.outdir
-        if not os.path.exists(outdir):
-            os.mkdir(outdir)
-        # fail if ANY of expected outputs already exist
-        io.check_image_level_outputs_exist(outdir, fname_in, gzip=True,
-                                           cube_index=cube_index)
-
-    exp = io.load_exposure(fname_in, cube_index=cube_index)
-
-    print('Attempting to compute basic statistics of raw pixel data')
-    imstats = io.gather_pixel_stats(exp)
-
-    # create data quality bitmasks
-    exp.create_all_bitmasks()
-
-    # go from "raw" images to "reduced" images
-    exp.calibrate_pixels()
-
-    # calculate sky brightness in mag per sq asec
-    exp.estimate_all_sky_mags(careful_sky=args.careful_sky)
-    exp.estimate_all_sky_sigmas(careful_sky=args.careful_sky)
-
-    par = common.ci_misc_params()
-
-    if not args.no_cataloging:
-        catalogs = exp.all_source_catalogs()
-
-        for extname, cat in catalogs.items():
-            if cat is not None:
-                util.create_det_ids(cat, extname, fname_in,
-                                    cube_index=cube_index)
-
-        # reformat the output catalogs into a single merged astropy Table
-        catalog = io.combine_per_camera_catalogs(catalogs)
-
-        # run astrometric recalibration
-        print('Attempting astrometric recalibration relative to Gaia DR2')
-        astr = wcs.recalib_astrom(catalog, fname_in)
-        exp.update_wcs(astr)
-        exp.recompute_catalog_radec(catalog)
-        
-        if (not args.no_gaia_xmatch) and (par['gaia_env_var'] in os.environ):
-            print('Attempting to identify Gaia cross-matches')
-            catalog = io.append_gaia_crossmatches(catalog)
-        
-    # try to write image-level outputs if outdir is specified
-
-    if write_outputs:
-        if not args.skip_image_outputs:
-            print('Attempting to write image-level outputs to directory : ' + 
-                  outdir)
-            # could add command line arg for turning off gzip compression
-            io.write_image_level_outputs(exp, outdir, fname_in, gzip=True,
-                                         cube_index=cube_index)
-
-        if not args.no_cataloging:
-            io.write_exposure_source_catalog(catalog, outdir, fname_in,
-                                             cube_index=cube_index)
-        # make this work correctly in the case that --no_cataloging is set
-        io.write_ccds_table(imstats, catalog, exp, outdir, fname_in,
-                            cube_index=cube_index)
-
-    print('Successfully finished reducing ' + fname_in)
-
-    print('GFA reduction pipeline completed at: ' + str(datetime.utcnow()) + 
-          ' UTC')
+    _proc(fname_in, outdir=args.outdir, careful_sky=args.careful_sky,
+          no_cataloging=args.no_cataloging, no_gaia_xmatch=args.no_gaia_xmatch,
+          cube_index=args.cube_index,
+          skip_image_outputs=args.skip_image_outputs)
