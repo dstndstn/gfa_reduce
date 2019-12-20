280f7079efc88daab45b4a2c4dab6a28b8f3b34e
Aaron Meisner <ameisner@fas.harvard.edu>
2019-12-19 18:48:47 -0800

    initial; loop over all image slices in a guider cube; severe I/O inefficiencies

---
+++ py/ci_reduce/cube_proc.py
@@ -0,0 +1,31 @@
+import argparse
+from ci_reduce.gfa_red import _proc
+import astropy.io.fits as fits
+import os
+
+if __name__ == "__main__":
+    descr = 'run gfa_reduce pipeline on all GFA guide cube slices'
+
+    parser = argparse.ArgumentParser(description=descr)
+    parser.add_argument('fname_in', type=str, nargs=1)
+
+    parser.add_argument('--outdir', default=None, type=str,
+                        help='directory to write outputs in')
+
+    args = parser.parse_args()
+    
+    fname_in = args.fname_in[0]
+
+    assert(os.path.exists(fname_in))
+    
+    h = fits.getheader(fname_in, extname='GUIDER')
+
+    nframes = h['FRAMES']
+    
+    for i in range(nframes):
+        # note the hardcoding of certain arguments, especially 
+        # skipping write-out of image-level outputs (saves disk space...)
+        _proc(fname_in, outdir=args.outdir,
+              careful_sky=False, no_cataloging=False,
+              no_gaia_xmatch=False, cube_index=i, skip_image_outputs=True,
+              realtime=False)
