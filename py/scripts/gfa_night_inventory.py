f0b18cbe075a63e63f68df81f831cb0a12dadb82
Aaron Meisner <ameisner@fas.harvard.edu>
2020-01-18 20:50:18 -0700

    option to also print out IMAGECAM

--- py/scripts/gfa_night_inventory.py
+++ py/scripts/gfa_night_inventory.py
@@ -4,8 +4,8 @@ import argparse
 import astropy.io.fits as fits
 import glob
 
-def _printouts(night, full_filename=False):
-    basedir = '/project/projectdirs/desi/spectro/data'
+def _printouts(night, full_filename=False, imagecam=False):
+    basedir = '/exposures/desi'
     nightdir = basedir + '/' + night
     flist = glob.glob(nightdir + '/*/gfa*.fits.fz')
 
@@ -14,7 +14,7 @@ def _printouts(night, full_filename=False):
     for f in flist:
         h = fits.getheader(f, extname='GFA')
         fp = (f.replace(basedir + '/', '') + '   ') if not full_filename else f + '   '
-        print(fp, h['FLAVOR'].ljust(10, ' '), '{:.1f}'.format(h['EXPTIME']).ljust(10, ' '), h['PROGRAM'])
+        print(fp, h['FLAVOR'].ljust(10, ' '), '{:.1f}'.format(h['EXPTIME']).ljust(10, ' '), h['PROGRAM'].ljust(40), '' if not imagecam else h['IMAGECAM'])
 
 if __name__ == "__main__":
     descr = 'print information about GFA prescan/overscan bad pixels'
@@ -27,9 +27,13 @@ if __name__ == "__main__":
                         action='store_true', 
                         help='print full gfa*.fz filename path')
 
+    parser.add_argument('--imagecam', default=False,
+                        action='store_true',
+                        help='also print IMAGECAM')
+
 
     args = parser.parse_args()
 
     night = args.night[0]
 
-    _printouts(night, full_filename=args.full_filename)
+    _printouts(night, full_filename=args.full_filename, imagecam=args.imagecam)
