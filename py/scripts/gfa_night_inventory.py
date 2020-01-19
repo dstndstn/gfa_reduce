9f3b4cfedf1e16571620d2025a360009ed1ecf91
Aaron Meisner <ameisner@fas.harvard.edu>
2020-01-18 21:29:15 -0700

    add option to print out guide cube inventory rather than gfa*.fits.fz inventory

--- py/scripts/gfa_night_inventory.py
+++ py/scripts/gfa_night_inventory.py
@@ -4,20 +4,25 @@ import argparse
 import astropy.io.fits as fits
 import glob
 
-def _printouts(night, full_filename=False, imagecam=False):
+def _printouts(night, full_filename=False, imagecam=False, guider=False):
     basedir = '/exposures/desi'
     nightdir = basedir + '/' + night
-    flist = glob.glob(nightdir + '/*/gfa*.fits.fz')
+    pattern = 'gfa*.fits.fz' if not guider else 'guide-????????.fits.fz'
+    flist = glob.glob(nightdir + '/*/' + pattern)
 
     flist.sort()
 
+    extname = 'GFA' if not guider else 'GUIDER'
     for f in flist:
-        h = fits.getheader(f, extname='GFA')
+        h = fits.getheader(f, extname=extname)
         fp = (f.replace(basedir + '/', '') + '   ') if not full_filename else f + '   '
-        print(fp, h['FLAVOR'].ljust(10, ' '), '{:.1f}'.format(h['EXPTIME']).ljust(10, ' '), h['PROGRAM'].ljust(40), '' if not imagecam else h['IMAGECAM'])
+        if not guider:
+            print(fp, h['FLAVOR'].ljust(10, ' '), '{:.1f}'.format(h['EXPTIME']).ljust(10, ' '), h['PROGRAM'].ljust(40), '' if not imagecam else h['IMAGECAM'])
+        else:
+            print(fp, str(h['FRAMES']).ljust(10, ' '), h['PROGRAM'].ljust(40))
 
 if __name__ == "__main__":
-    descr = 'print information about GFA prescan/overscan bad pixels'
+    descr = 'print inventory of GFA data for a night'
     parser = argparse.ArgumentParser(description=descr)
 
     parser.add_argument('night', type=str, nargs=1,
@@ -31,9 +36,12 @@ if __name__ == "__main__":
                         action='store_true',
                         help='also print IMAGECAM')
 
+    parser.add_argument('--guider', default=False, 
+                        action='store_true', 
+                        help='guide-????????.fits.fz rather than gfa*.fits.fz')
 
     args = parser.parse_args()
 
     night = args.night[0]
 
-    _printouts(night, full_filename=args.full_filename, imagecam=args.imagecam)
+    _printouts(night, full_filename=args.full_filename, imagecam=args.imagecam, guider=args.guider)
