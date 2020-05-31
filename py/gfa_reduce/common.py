b72c589b74795142836eb654dddac47752a52c39
Aaron Meisner <ameisner@fas.harvard.edu>
2020-05-31 02:42:13 -0700

    clean-up

--- py/gfa_reduce/common.py
+++ py/gfa_reduce/common.py
@@ -76,48 +76,6 @@ def gfa_camera_readnoise(extname):
     r = gfa_camera_readnoise_dict()
     return r[extname]
 
-def valid_image_extnum_list(fz=True):
-    """ 
-    When fzipped, valid image extension numbers are 1-5 inclusive.
-    When fzipped, there is also a dummy zeroth extension.
-    When not fzipped, the valid image extensions are 0-4 inclusive.
-    When not fzipped, there is no dummy zeroth extension.
-
-    astropy.io.fits has weird (buggy?) behavior for fzipped files:
-        - fits.getheader with ext=0 returns the dummy ext=0 header.
-        - fits.getdata with ext=0 and header=True returns the ext=1 header and
-          ext=1 image data.
-
-    I want image extension numbers that are valid for both getheader and
-    getdata, so for the fzipped case, I will assert that the valid image
-    extension numbers are 1-5 inclusive and that ext=0 is invalid.
-
-    hdul.info()
-    Filename: FORDK_NOBIN-00000001.fits.fz
-    No.    Name      Ver    Type      Cards   Dimensions   Format
-      0  CI            1 PrimaryHDU       15   ()
-      1  CIN           1 CompImageHDU     43   (3072, 2048)   int16
-      2  CIW           1 CompImageHDU     43   (3072, 2048)   int16
-      3  CIC           1 CompImageHDU     43   (2048, 3072)   int16
-      4  CIE           1 CompImageHDU     43   (3072, 2048)   int16
-      5  CIS           1 CompImageHDU     43   (3072, 2048)   int16
-
-    hdul.info()
-    Filename: FORDK_NOBIN-00000001.fits
-    No.    Name      Ver    Type      Cards   Dimensions   Format
-      0 CIN            1 PrimaryHDU      47   (3072, 2048)    int16
-      1 CIW            1 ImageHDU        42   (3072, 2048)    int16
-      2 CIC            1 ImageHDU        42   (2048, 3072)    int16
-      3 CIE            1 ImageHDU        42   (3072, 2048)    int16
-      4 CIS            1 ImageHDU        42   (3072, 2048)    int16
-
-    Note that the swapped dimensions of CIC above are a bug in the sample
-    data, and that the number of header cards will presumably be changing
-    relative to the preliminary sample data.
-    """
-
-    return (np.arange(1, 6) if fz else np.arange(0, 5))
-
 def valid_image_extname_list():
     return ['GUIDE0', 'GUIDE2', 'GUIDE3', 'GUIDE5', 'GUIDE7', 'GUIDE8']
 
@@ -189,14 +147,6 @@ def gfa_extname_to_gfa_number(extname):
 
     return d[extname]
 
-def ci_extnum_to_extname(extnum, fz=True):
-
-    assert(is_valid_extnum(extnum, fz=fz))
-
-    d = ci_extnum_to_extname_dict(fz=fz)
-
-    return d[extnum]
-
 def ci_number_to_ci_extname(num):
     """
     CI number refers to CI# as labeled on DESI-3347 page 4
@@ -215,8 +165,6 @@ def ci_number_to_extnum(num, fz=True):
     d = ci_extname_to_extnum_dict(fz=fz)
     return d[extname]
 
-def is_valid_extnum(extnum, fz=True):
-    return (extnum in valid_image_extnum_list(fz=fz))
 
 def is_valid_image_extname(extname):
     return (extname in valid_image_extname_list())
