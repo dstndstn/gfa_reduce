76e6a3173095284ea63e7da466e0f987122d0ce3
Aaron Meisner <ameisner@fas.harvard.edu>
2020-05-30 23:07:58 -0700

    more renaming

--- py/gfa_reduce/imred/load_calibs.py
+++ py/gfa_reduce/imred/load_calibs.py
@@ -12,55 +12,55 @@ def remove_overscan(image):
     return _image
 
 
-def read_bias_image(ci_extname):
-    assert(common.is_valid_extname(ci_extname))
+def read_bias_image(extname):
+    assert(common.is_valid_extname(extname))
 
     par = common.gfa_misc_params()
     bias_fname = os.path.join(os.environ[par['etc_env_var']], \
                               par['master_bias_filename'])
 
     print('Attempting to read master bias : ' + bias_fname + 
-          ', extension name : ' + ci_extname)
+          ', extension name : ' + extname)
 
     assert(os.path.exists(bias_fname))
 
-    bias = fits.getdata(bias_fname, extname=ci_extname)
+    bias = fits.getdata(bias_fname, extname=extname)
 
     bias = remove_overscan(bias)
     return bias
 
-def read_flat_image(ci_extname):
+def read_flat_image(extname):
     # at some point should add option to return master flat's
     # inverse variance as well
-    assert(common.is_valid_extname(ci_extname))
+    assert(common.is_valid_extname(extname))
 
     par = common.gfa_misc_params()
     flat_fname = os.path.join(os.environ[par['etc_env_var']], \
                               par['master_flat_filename'])
 
     print('Attempting to read master flat : ' + flat_fname + 
-          ', extension name : ' + ci_extname)
+          ', extension name : ' + extname)
 
     assert(os.path.exists(flat_fname))
 
-    flat = fits.getdata(flat_fname, extname=ci_extname)
+    flat = fits.getdata(flat_fname, extname=extname)
 
     flat = remove_overscan(flat)
     return flat
 
-def read_static_mask_image(ci_extname):
-    assert(common.is_valid_extname(ci_extname))
+def read_static_mask_image(extname):
+    assert(common.is_valid_extname(extname))
 
     par = common.gfa_misc_params()
     mask_fname = os.path.join(os.environ[par['etc_env_var']], \
                               par['static_mask_filename'])
 
     print('Attempting to read static bad pixel mask : ' + mask_fname + 
-          ', extension name : ' + ci_extname)
+          ', extension name : ' + extname)
 
     assert(os.path.exists(mask_fname))
 
-    mask = fits.getdata(mask_fname, extname=ci_extname)
+    mask = fits.getdata(mask_fname, extname=extname)
 
     mask = remove_overscan(mask)
     return mask
